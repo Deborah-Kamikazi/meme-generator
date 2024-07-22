d42019390eb6d7aa41b0c1128fcb2f4db228319d
Kamikazi-Deborah <deborahkamikazi9@gmail.com>
2024-07-22 10:33:29 +0200

    get memes

---
+++ memegenerator/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <section className=' h-[550px] flex flex-col items-center'>
+        <nav className='bg-[#A626D3] w-[550px] flex items-center h-[65px] px-3'>
+          <img src="/Troll Face.svg" alt="" className='size-9' />
+          <p className='text-white font-semibold text-xl'>Meme Generator</p>
+        </nav>
+   </section>
+    </>
+  )
+}
+
+export default App
